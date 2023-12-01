0e58e34545863c46208596ed5734e96eb019c946
ArnejoKeinth <202280176@psu.palawan.edu.ph>
2023-12-01 16:37:46 +0800

    admin.py improvement

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from.models import PokemonCard
-from.models import Trainer
+from .models import PokemonCard, Trainer
+
+@admin.register(PokemonCard)
+class PokemonCardAdmin(admin.ModelAdmin):
+    list_display = ("name", "rarity")
+    search_fields = ("name",)
 
 @admin.register(Trainer)
 class TrainerAdmin(admin.ModelAdmin):
     list_display = ("name", "birthdate", "location", "email")
     search_fields = ("name", "location",)
-
-admin.site.register(PokemonCard)
